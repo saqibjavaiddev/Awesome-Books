63da538ef75c360c5f0ab442a5542f0486899190
msaqibjavaid <duckyprogrammer@gmail.com>
2022-06-28 15:20:10 +0500

    Fixed js linter errors

--- assets/jsclass.js
+++ assets/jsclass.js
@@ -1,63 +1,67 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable getter-return */
 const bookForm = document.querySelector('#book-form');
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const booksList = document.querySelector('#book-list');
-  
-class Book {  
+
+class Book {
   constructor(title, author, id) {
     this.title = title;
     this.author = author;
-    if (id !== undefined) {  
-      this.id =  id;
+    if (id !== undefined) {
+      this.id = id;
     }
   }
 
-    static books =  JSON.parse(localStorage.getItem('books')) || [];
+    static books = JSON.parse(localStorage.getItem('books')) || [];
 
     add() {
-      Book.books.push(this);  
-      localStorage.setItem('books', JSON.stringify(Book.books));  
-    }  
+      Book.books.push(this);
+      localStorage.setItem('books', JSON.stringify(Book.books));
+    }
 
-    remove() {  
+    remove() {
       const removeId = parseInt(this.id, 10);
-      console.log(removeId);
       let booklist = Book.books;
-      booklist =  booklist.filter((element, index) => index !== removeId);
-      Book.books =  booklist;  
+      booklist = booklist.filter((element, index) => index !== removeId);
+      Book.books = booklist;
       localStorage.setItem('books', JSON.stringify(booklist));
-      window.location.reload();  
-    }  
-    get theDisplay() {  
-      let bookHtml = '';  
+      window.location.reload();
+    }
+
+    // eslint-disable-next-line getter-return
+    // eslint-disable-next-line class-methods-use-this
+    get theDisplay() {
+      let bookHtml = '';
       Book.books = JSON.parse(localStorage.getItem('books'));
       if (Book.books === null) {
         Book.books = [];
       }
       Book.books.forEach((item, index) => {
-        bookHtml += ` 
-          <div class="book"> 
-          <p class="book-info">"${item.title}" by </p>
+        bookHtml += `
+          <div class="onebook">
+          <p class="book-info">"${item.title}" </p>
           <p class="book-info"> ${item.author}</p>
           <button type="button" class="remove-btn" id="${index}">Remove</button>
-          </div>  
-          `;  
-      });   
+          </div>
+          `;
+      });
 
-      booksList.innerHTML =  bookHtml;
+      booksList.innerHTML = bookHtml;
     }
 }
 
-bookForm.addEventListener('submit',   () => {
-  const newBook = new Book(bookTitle.value,   bookAuthor.value);
+bookForm.addEventListener('submit', () => {
+  const newBook = new Book(bookTitle.value, bookAuthor.value);
   newBook.add();
 });
 
-const getBooks = new Book(bookTitle.value,   bookAuthor.value);
+const getBooks = new Book(bookTitle.value, bookAuthor.value);
 getBooks.theDisplay;
 
-const removeBtn = document.querySelectorAll('.remove-btn' );
-removeBtn.forEach((item) =>  item.addEventListener('click', ()  =>  {
-  const removeBook = new  Book(bookTitle.value, bookAuthor.value, item.id);
+const removeBtn = document.querySelectorAll('.remove-btn');
+removeBtn.forEach((item) => item.addEventListener('click', () => {
+  const removeBook = new Book(bookTitle.value, bookAuthor.value, item.id);
   removeBook.remove();
-})); 
+}));
